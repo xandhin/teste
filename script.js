847fdba9f50f15983e2b7d15e72e221c448a0db0
xandhin <42303439+xandhin@users.noreply.github.com>
2023-11-14 09:23:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// helloWorld.js
+
+// Function to display an alert with the message "Hello, World!"
+function showAlert() {
+  alert("Hello, World!");
+}
+
+// Call the showAlert function
+showAlert();
