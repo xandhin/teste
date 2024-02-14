3b5fe56216b3cc2332c1bc3abced7c831b052e58
xandhin <42303439+xandhin@users.noreply.github.com>
2024-02-14 14:53:29 -0300

    Create hide.js

---
+++ hide.js
@@ -0,0 +1,14 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var navElement = document.querySelector("nav.navbar");
+        if (navElement) {
+            navElement.style.display = "none";
+        }
+    });
+    document.addEventListener("DOMContentLoaded", function() {
+        var navElement = document.querySelector("nav.navbar-default.navbar-static-side");
+        if (navElement) {
+            navElement.style.display = "none";
+        }
+    });
+</script>
