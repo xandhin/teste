a91dfd8f5b263760e9849f8ee6b427695a5a2963
xandhin <42303439+xandhin@users.noreply.github.com>
2024-02-16 09:12:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// helloWorld.js
-
-// Function to display an alert with the message "Hello, World!"
-function showAlert() {
-  alert("Hello, World!");
-}
-
-// Call the showAlert function
-showAlert();
+alert(32434)
