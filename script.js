7f11a6c773c1cf80a065608ceee135e450db52d6
xandhin <42303439+xandhin@users.noreply.github.com>
2024-02-16 13:38:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById("divMenu").style.display = "none";
+var iframe = document.getElementById('grpe-fra');
+var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+var divMenu = iframeDoc.getElementById('divMenu');
+
+// Check if the divMenu exists before attempting to hide it
+if (divMenu) {
+    divMenu.style.display = 'none';
+} else {
+    console.error('divMenu not found in the iframe.');
+}
